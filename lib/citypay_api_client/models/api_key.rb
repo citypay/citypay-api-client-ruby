a9d4634ea17b5ac14ded05c4ba200ac0ef5518ce
Gary Feltham <gary.feltham@citypay.com>
2020-08-04 15:59:24 +0100

    initial commit

--- lib/citypay_api_client/models/api_key.rb
+++ lib/citypay_api_client/models/api_key.rb
@@ -28,7 +28,7 @@ module CityPayApiClient
       @datetime = datetime
     end
 
-    private def hexToBytes(str)
+    private def hex_to_bytes(str)
       str.scan(/../).map { |x| x.hex.chr }.join.bytes
     end
 
@@ -38,7 +38,7 @@ module CityPayApiClient
       message = []
       message.push(*@client_id.bytes)
       message.push(*@nonce)
-      message.push(*hexToBytes(ds))
+      message.push(*hex_to_bytes(ds))
       # message = message.join
       digest = OpenSSL::HMAC.digest('sha256', @licence_key, message.pack("c*"))
 
@@ -48,18 +48,7 @@ module CityPayApiClient
       dest.push(*@nonce.each_entry.map { |b| '%02X' % (b & 0xFF) }.join.upcase.bytes)
       dest.push(*"\x3A".bytes)
       dest.push(*digest.bytes)
-
-      # dt = datetime.strptime("2020-01-01 09:23:12", "%Y-%m-%d %H:%M:%S")
-      # key = api_key_generate_for("PC2", "7G79TG62BAJTK669", bytearray.fromhex("acb875aef083de292299bd69fcdeb5c5"), dt)
-      # self.assertEqual("UEMyOkFDQjg3NUFFRjA4M0RFMjkyMjk5QkQ2OUZDREVCNUM1Ol6Q3tCMPsYvqNhFelRD2zQHYlZquBGCY/6ZpZ0AngTF",key)
       Base64.strict_encode64(dest.pack("c*"))
-
-      # Array(80, 67, 50,
-      # 58,
-      # 65, 67, 66, 56, 55, 53, 65, 69, 70, 48, 56, 51, 68, 69, 50, 57, 50, 50, 57, 57, 66, 68, 54, 57, 70, 67, 68, 69, 66, 53, 67, 53,
-      # 58,
-      # 94, -112, -34, -48, -116, 62, -58, 47, -88, -40, 69, 122, 84, 67, -37, 52, 7, 98, 86, 106, -72, 17, -126, 99, -2, -103, -91, -99, 0, -98, 4, -59
-      # )
 
     end
 
